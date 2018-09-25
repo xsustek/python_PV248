0dcd5f186bddbc76f84972050ec18fea60939739
Milan Šůstek <milan.sustek@gmail.com>
2018-09-25 15:54:19 +0200

    function, sorted output

--- 01-text/stat.py
+++ 01-text/stat.py
@@ -2,14 +2,21 @@ import sys
 import re
 from collections import Counter
 f = open(sys.argv[1], 'r', encoding='utf8')
-r = re.compile(r"Composer: (.*)")
+r = re.compile(r"Composer: (.*).*\(?.*\)?")
 
-counter = Counter()
 
-for line in f:
-    m = r.match(line)
-    if m is not None:
-        counter[m.group(1)] += 1
 
-for k, v in counter.items():
-    print(k, v)
+def composer():
+    counter = Counter()
+
+    for line in f:
+        m = r.match(line)
+        if m is not None and m.group(1):
+            names = re.sub(r"\(.*\)", "", m.group(1))
+            for name in names.split(";"):
+                counter[name.strip()] += 1
+
+    for k, v in sorted(counter.items(), key=lambda kv: kv[1]):
+        print(k, v)
+
+composer()
