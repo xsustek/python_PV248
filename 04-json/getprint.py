2b594f0ec469dcc52d174dbb41593b2c0cbef9fc
Milan Šůstek <milan.sustek@gmail.com>
2018-10-24 00:14:51 +0200

    getprint done

--- 04-json/getprint.py
+++ 04-json/getprint.py
@@ -2,6 +2,15 @@ import sys
 import sqlite3
 import json
 
+def create_composer(row):
+    res = {}
+    res["name"] = row[0]
+    if row[1] is not None:
+        res["born"] = row[1]
+    if row[2] is not None:
+        res["died"] = row[2]
+    return res
+
 database = "scorelib.dat"
 
 print_number = sys.argv[1]
@@ -12,6 +21,6 @@ cur = con.cursor()
 
 query = cur.execute("SELECT person.name, person.born, person.died FROM 'print' JOIN 'edition' ON print.edition = edition.id JOIN 'score' ON edition.score = score.id JOIN 'score_author' ON score.id = score_author.score JOIN 'person' ON score_author.composer = person.id where print.id = ?", (print_number,))
 
-res = list(map(lambda c: {"name": c[0], "born": c[1], "died": c[2]}, query))
+res = list(map(create_composer, query))
 
-print(json.dumps(res, indent=4))
+print(json.dumps(res, indent=4, ensure_ascii=False))
