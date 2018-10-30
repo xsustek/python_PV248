6089f79a906b176fc7ccba81d88140b8ed0a62db
Milan Šůstek <milan.sustek@gmail.com>
2018-10-30 23:59:26 +0100

    fix

--- 05-linalg/eqn.py
+++ 05-linalg/eqn.py
@@ -29,7 +29,7 @@ def line_into_list(line):
     splits = str(line).split(" = ")
     m = sorted(re.findall(r"[+-]? ?\d?[a-z]", splits[0]), key=lambda f: f[f.__len__() - 1])
 
-    dic = dict.fromkeys(list(string.ascii_lowercase), 0)
+    dic = {}
     for c in m:
         coef = c[:-1].replace(' ', '')
         if not re.match(r"[+-]?\d", coef):
@@ -68,15 +68,25 @@ eqs = list(map(line_into_list, f))
 
 arr = []
 vals = []
+key_set = set()
+
+for i in eqs:
+    for k in i.left.keys():
+        key_set.add(k)
+
+key_set = list(sorted(key_set))
+
 for i in eqs:
     vals.append(i.right)
-    arr.append(list(i.left.values()))
+    eq_arr = []
+    for k in key_set:
+        if i.left.__contains__(k):
+            eq_arr.append(i.left[k])
+        else:
+            eq_arr.append(0)
+    arr.append(eq_arr)
 
-min = find_start(arr)
-max = find_end(arr)
-w = slice_arr(arr, min, max)
-
-narr = array(w)
+narr = array(arr)
 nvals = array(vals)
 
 aug_rank = linalg.matrix_rank(column_stack((narr, nvals)))
@@ -85,16 +95,16 @@ coef_rank = linalg.matrix_rank(narr)
 if coef_rank < aug_rank:
     print_eqs(eqs)
     print("no solution")
-elif coef_rank == aug_rank and coef_rank < w.__len__():
+elif coef_rank == aug_rank and coef_rank < arr.__len__():
     print_eqs(eqs)
-    print("solution space dimension:", w.__len__() - coef_rank)
+    print("solution space dimension:", arr.__len__() - coef_rank)
 else:
     print_eqs(eqs)
     res = linalg.solve(narr, nvals)
     solution = ""
     for i, e in enumerate(nditer(res.T)):
-        keys = list(eqs[0].left.keys())
-        solution += str(keys[i]) + " = " + str(e) + ", "
+        keys = list(eqs[i].left.keys())
+        solution += str(key_set[i]) + " = " + str(e) + ", "
 
 
     print("solution: " + solution.strip(", "))
