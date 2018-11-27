8aba7511925e3cae6ca027d782b4382ec6b3e606
Milan Šůstek <milan.sustek@gmail.com>
2018-11-27 23:22:34 +0100

    09 first try

---
+++ 09-http/http-forward.py
@@ -0,0 +1,57 @@
+import urllib.request as req
+from aiohttp import web
+import aiohttp
+import asyncio
+import sys
+import json
+
+
+def is_json(myjson):
+    try:
+        json_object = json.loads(myjson)
+    except ValueError as e:
+        return False
+    return True
+
+
+port = sys.argv[1]
+host = "http://" + sys.argv[2]
+
+session = aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=1))
+
+
+async def handleGet(request):
+    heads = dict(request.headers)
+    try:
+        async with session.get(host + request.raw_path, headers=heads) as resp:
+            res = {
+                "code": resp.status,
+                "headers": dict(resp.headers)
+            }
+            content = await resp.text()
+            if is_json(content):
+                res["json"] = json.loads(content)
+            else:
+                res["content"] = str(content)
+            return web.Response(text=json.dumps(res), content_type=resp.content_type)
+    except asyncio.TimeoutError:
+        return web.Response(text="timeout")
+
+
+async def handlePost(request):
+    if is_json(await request.read()):
+        js = await request.json()
+        method = js["type"]
+        url = js["url"]
+        async with session.request(method if method is not None else "GET", url, headers=js["headers"]) as resp:
+
+            return web.Response(text="OK")
+
+
+app = web.Application()
+app.add_routes([web.get('/{param:.*}', handleGet),
+                web.post('/{params:.*}', handlePost)])
+
+web.run_app(app, port=port)
+
+session.close()
