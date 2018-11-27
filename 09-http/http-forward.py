bc1cea7beac167be5519b8a614aedf91c1b3eefc
Milan Šůstek <milan.sustek@gmail.com>
2018-11-28 00:07:02 +0100

    09 -next

--- 09-http/http-forward.py
+++ 09-http/http-forward.py
@@ -41,12 +41,25 @@ async def handleGet(request):
 async def handlePost(request):
     if is_json(await request.read()):
         js = await request.json()
-        method = js["type"]
-        url = js["url"]
-        async with session.request(method if method is not None else "GET", url, headers=js["headers"]) as resp:
-
-            return web.Response(text="OK")
-
+        if js.__contains__("url"):
+            url = js["url"]
+            method = js["type"] if js.__contains__("type") else "GET"
+            timeout = js["timeout"] if js.__contains__("timeout") else 1
+            try:
+                async with session.request(method, url, headers=js["headers"], timeout=timeout) as resp:
+                    res = {
+                    "code": resp.status,
+                    "headers": dict(resp.headers)
+                }
+                    content = await resp.text()
+                    if is_json(content):
+                        res["json"] = json.loads(content)
+                    else:
+                        res["content"] = str(content)
+                    return web.Response(text=json.dumps(res), content_type=resp.content_type)
+            except asyncio.TimeoutError:
+                return web.Response(text="timeout")
+    return web.Response(text=json.dumps({"code":"invalid json"}))
 
 app = web.Application()
 app.add_routes([web.get('/{param:.*}', handleGet),
